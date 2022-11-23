374df02823e7f844c9ada716644d451460f946f4
netantv@gmail.com <netantv@gmail.com>
2022-11-23 16:27:20 +0200

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled1 = () => {
-  return <View style={_styles.dHljoKEj}>
+const Untitled2 = () => {
+  return <View style={_styles.MxDxBwyo}>
       </View>;
 };
 
-export default Untitled1;
+export default Untitled2;
 
 const _styles = StyleSheet.create({
-  dHljoKEj: {
+  MxDxBwyo: {
     backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
